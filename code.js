9f8f11c639dd9f4c3d1bda2ae61f434bca1c46c5
abdurrahman <rajab-2@windowslive.com>
2019-12-16 14:57:25 +0300

    Quiz 0.04
    Add HSL,

--- code.js
+++ code.js
@@ -14,9 +14,20 @@ function getColour(target) {
   return numArr;
 }
 
-function compare(source,target,hata=50) {
+function arrToInt(arr){
+  return arr.map(x => Number.parseInt(x, 10));
+}
+function compare(source,target,hata=50,mode) {
     let s = getColour(source)
     let t = getColour(target)
+    if(mode=="hsl"){
+      s= rgbToHsl(...s);
+      t= rgbToHsl(...t);
+      s = s.map((x) => {return x*100})
+      t = t.map((x) => {return x*100})
+
+    }
+    
     let result =[];
     let biRes=[];
     for (let i = 0; i < s.length; i++) {
@@ -38,6 +49,38 @@ function compare(source,target,hata=50) {
     return [result,biRes]
 }
 
-function greenKey(){
-    
+/**
+ * https://gist.github.com/mjackson/5311256
+ * Converts an RGB color value to HSL. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and l in the set [0, 1].
+ *
+ * @param   Number  r       The red color value
+ * @param   Number  g       The green color value
+ * @param   Number  b       The blue color value
+ * @return  Array           The HSL representation
+ */
+function rgbToHsl(r, g, b) {
+  r /= 255, g /= 255, b /= 255;
+
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, l = (max + min) / 2;
+
+  if (max == min) {
+    h = s = 0; // achromatic
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+    switch (max) {
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
+
+    h /= 6;
+  }
+
+  return [ h, s, l ];
 }
